a154c24a2aba9bfdd61c25ce47dd3d66ba1c728c
shahnawroz <sh.nawrose@gmail.com>
2022-06-27 16:30:59 +0600

    frontend form validation

--- src/App.js
+++ src/App.js
@@ -1,116 +1,13 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React from 'react'
+import Form from "./component/form"
 
-export default function FormPropsTextFields() {
+function App() {
   return (
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-      style={{   display: "flex",
-      flexDirection: 'column',
-          justifyContent: "center",
-          alignItems: "center",
-          justifycontent: "space-between"
-        
-       }}
-    >
-      
-          <div>
-        <h3>Invoice Form</h3>
-        </div>
-        <div>
-        <h3>Invoice Form</h3>
-        
-      <div>
-       
-        <TextField
-          required
-          id="outlined-required"
-          label="Company Name"
-         
-        />
-      
-      </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Company Email"
-        
-        />
-      
-      </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Company Phone No"
-         
-        />
-      
-      </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Company Address"
-         
-        />
-      
-      </div>
-      </div>
-      <div>
-        <h3>Client Company Informations</h3>
-      
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Client Company Name"
-         
-        />
-      
-      </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Client Company Email"
-         
-        />
-      
-      </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Client Company Phone No"
-         
-        />
-      
-      </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Client Company Address"
-         
-        />
-      
-      </div>
-      </div>
-      <br/>
-    
+    <div>
+      <Form/>
 
-      <Button variant="contained"  >Submit</Button>
-    <br/>
-    <br/>
-    </Box>
-  );
+    </div>
+  )
 }
+
+export default App
